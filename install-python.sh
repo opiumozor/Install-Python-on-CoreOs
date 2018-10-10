e8b75eaea966a8c87086d3a048c1df84dd477edf
Alex Bernard <alexis.bernard33@gmail.com>
2018-10-10 14:02:22 +0200

    python 3.6

--- install-python.sh
+++ install-python.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -uxe
 
-VERSION=2.7.14.2717
-PACKAGE=ActivePython-${VERSION}-linux-x86_64-glibc-2.12-404899.tar.gz
+VERSION=3.6.0.3600
+PACKAGE=ActivePython-${VERSION}-linux-x86_64-glibc-2.3.6-401834
 
 # make directory
-mkdir -p /opt/bin
+mkdir -p /opt
 cd /opt
 
 wget http://downloads.activestate.com/ActivePython/releases/${VERSION}/${PACKAGE}.tar.gz
@@ -12,8 +12,12 @@ tar -xzvf ${PACKAGE}.tar.gz
 
 mv ${PACKAGE} apy && cd apy && ./install.sh -I /opt/python/
 
+rm /opt/${PACKAGE}.tar.gz
+rm -rf /opt/apy
+
 ln -sf /opt/python/bin/easy_install /opt/bin/easy_install
 ln -sf /opt/python/bin/pip /opt/bin/pip
 ln -sf /opt/python/bin/python /opt/bin/python
 ln -sf /opt/python/bin/python /opt/bin/python2
 ln -sf /opt/python/bin/virtualenv /opt/bin/virtualenv
+
